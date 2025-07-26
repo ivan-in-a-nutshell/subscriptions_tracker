a086bf9da433049f2bbaf69df52810f0fe5e2986
Ivan Zarytskyi <ivan.zarytskyi1991@gmail.com>
2025-07-26 10:45:08 +0300

    Super final

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, FloatField, DateField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 class SubscriptionForm(FlaskForm):
-    name = StringField('Service Name', validators=[DataRequired()])
-    cost = FloatField('Monthly Cost', validators=[DataRequired()])
+    name = StringField('Service Name', validators=[DataRequired()], render_kw={"placeholder": "Netflix, etc."})
+    cost = FloatField('Monthly Cost', validators=[DataRequired()], render_kw={"placeholder": "Enter price"})
     renewal_date = DateField(
         'Renewal Date', validators=[DataRequired()], format='%Y-%m-%d'
     )
     submit = SubmitField('Add Subscription')
 
 class EmailForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()], render_kw={"placeholder": "email@example.com"})
     submit = SubmitField('Update Email')
